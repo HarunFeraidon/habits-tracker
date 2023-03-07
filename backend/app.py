83562c9342e27f68eb8026631843561963d93a39
Harun Feraidon <hferaidon0@gmail.com>
2023-03-06 23:31:26 -0500

    redesign the db column named 'data'

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+import json
 
-from datetime import date
+from datetime import date, datetime, timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
@@ -13,24 +15,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/habits_
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 ma = Marshmallow(app)
 
 class Chart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     date_created = db.Column(db.Date, default=date.today())
-    data = db.Column(db.String(365))
+    data = db.Column(db.JSON)
 
     def __init__(self, title):
         self.title = title
         today = date.today()
         self.date_created = today
-        self.data = "0" * 10
+        self.data = self.init_data()
+    
+    def init_data(self):
+        one_year_ago = datetime.now() - timedelta(days=365)
+        data_list = []
+        for i in range(2):
+            date = one_year_ago + timedelta(days=i)
+            data_list.append({"value": 0, "date": date.strftime('%Y-%m-%d')})
+        return json.dumps(data_list)
 
     def complete_today(self):
-        lst = list(self.data)
-        lst[-1] = '1'
-        self.data = "".join(lst)
+        all_data = json.loads(self.data)
+        all_data[-1]['value'] = 1
+        self.data = json.dumps(all_data)
+
+    def shift_data(self):
+        all_data = json.loads(self.data)
+        all_data = all_data[1:]
+        all_data.append({"value": 0, "date": datetime.now().strftime('%Y-%m-%d')})
+        self.data = json.dumps(all_data)
+        
 
 class ChartSchema(ma.Schema):
     class Meta:
@@ -39,9 +57,13 @@ class ChartSchema(ma.Schema):
 chart_schema= ChartSchema()
 charts_schema= ChartSchema(many=True)
 
+@app.route('/inspect/<int:id>', methods=["GET"])
+def inspect(id):
+    chart = Chart.query.get_or_404(id)
+    return chart.data
+
 @app.route('/create/<title>', methods=["POST"])
 def create_chart(title):
-    # chart_title = request.json['title']
     new_chart = Chart(title=str(title))
     
     try:
@@ -73,7 +95,7 @@ def shift_data_left():
     with app.app_context():
         charts = Chart.query.all()
         for chart in charts:
-            chart.data = chart.data[1:] + '0'
+            chart.shift_data()
         db.session.commit()
 
 @app.route('/get/<int:id>', methods=['GET'])
