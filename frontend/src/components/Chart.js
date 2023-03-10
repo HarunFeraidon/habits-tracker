2d71b81e6626a8903e936c8e9bdc8bcec8ecea4f
Harun Feraidon <hferaidon0@gmail.com>
2023-03-09 23:50:03 -0500

    saving progress, frontend acting buggy

--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -5,24 +5,6 @@ function Chart(props) {
 
     const [data, setData] = useState(props.data)
 
-    /*
-<div className="item">
-            <h3>{props.title}</h3>
-            <CalendarChart data={data} year_start={props.year_start} year_end={props.year_end}/>
-            <div className='row'>
-                <div className='col-md-1'>
-                    <button className='btn btn-primary'
-                        onClick={() => handleUpdate(props.id)}> Complete today</button>
-                    <button className='btn btn-primary'
-                        onClick={() => handleDelete(props.id)}> Delete Chart</button>
-                </div>
-            </div>
-
-            <hr />
-
-        </div>
-    */
-
     function handleUpdate(id) {
         fetch(`/finish/${id}`, {
             method: "POST",
@@ -42,23 +24,23 @@ function Chart(props) {
     }
 
     return (
-        <div className="row">
-            <div className="col-2">
-                <div className="row">
-                    <div className="col-12">
+        <div className="row border">
+            <div className="col-2 border">
+                <div className="row border justify-content-around">
+                    <div className="col-12 border">
                         <h3>{props.title}</h3>
                     </div>
-                    <div className="col-12">
+                    <div className="col-12 border">
                         <button className='btn btn-primary'
                             onClick={() => handleUpdate(props.id)}> Complete today</button>
                     </div>
-                    <div className="col-12">
+                    <div className="col-12 border">
                         <button className='btn btn-primary'
                             onClick={() => handleDelete(props.id)}> Delete Chart</button>
                     </div>
                 </div>
             </div>
-            <div className="col-10">
+            <div className="col-10 border">
                 <CalendarChart data={data} year_start={props.year_start} year_end={props.year_end} />
             </div>
         </div>
